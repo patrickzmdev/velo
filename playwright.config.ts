d97a05a351ba11e4b5101dc8096cc712187fc5cf
Patrick <patrickzm290493@gmail.com>
2026-05-20 21:07:37 -0300

    ajuste aula locators

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './playwright/e2e',
+  timeout: 5000,
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
--- playwright/e2e/online.spec.ts
+++ playwright/e2e/online.spec.ts
@@ -1,8 +1,21 @@
 import { test, expect } from '@playwright/test';
 
-test('navega para a página inicial', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:5173');
-
   await expect(page).toHaveTitle(/Velô by Papito/);
+});
+
+test('navega para a página inicial', async ({ page }) => {
+  
   await expect(page.getByTestId('hero-section')).toBeVisible();
+});
+
+test('deve exibir o pedido encontrado', async ({ page }) => {
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toHaveText('Velô Sprint');
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
+  await page.getByPlaceholder('Ex: VLO-ABC123').fill('VLO-ULGOAW');
+  await expect(page.getByRole('button', { name: 'Buscar Pedido' })).toBeVisible();
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click();
+  await expect(page.locator('div p[data-testid="order-result-id"]')).toHaveText('VLO-ULGOAW');
 });
